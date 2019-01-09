e174fc1877a1bde192d940f28a0a4ce333aa33e3
Zer0-M <mjamil90@stuy.edu>
2019-01-09 13:05:41 -0500

    added docstrings and made it easier to input time

--- util/apihelp.py
+++ util/apihelp.py
@@ -1,6 +1,10 @@
 import datetime
 import requests
+DarkSkyKey="a0102af2cf0070050894dd64c8980654"
 def holidays(year,month):
+    '''
+        Gets a dictionary of holidays for a certain month and year
+    '''
     r=requests.get("https://holidayapi.pl/v1/holidays?country=US&year={0}&month={1}".format(year,month))
     try:
         data=r.json()
@@ -11,15 +15,29 @@ def holidays(year,month):
 #print(holidays(2019,1))
 
 def datify(time):
+    '''
+        This function accepts a datetime object and converts that into a string that will work with the Dark Sky API
+    '''
     t=str(time)
     t=t.replace(" ","T")
+    t=t.split(".")[0]
     return t
 def weather(location,time):
-    r=requests.get("https://api.darksky.net/forecast/a0102af2cf0070050894dd64c8980654/{0},{1},{2}".format(location[0],location[1],time))
+    '''
+        This function gets the weather and temperature for a certain place at a certain time via the Dark Sky API
+        The locations should be a tuple containting the longitude and latitude for example (latitude, longitude)
+        The time should be a datetime object
+    '''
+    time=datify(time)
+    print(time)
+    r=requests.get("https://api.darksky.net/forecast/{0}/{1},{2},{3}".format(DarkSkyKey,location[0],location[1],time))
     try:
         data=r.json()
     except:
         print("something went wrong")
-    return data
+    weather=[]
+    weather.append(data["currently"]["summary"])
+    weather.append(int(data["currently"]["temperature"]))
+    return weather
 loc=(40.717716,-74.014039)
-#print(weather(loc,"2019-01-09T12:29:17"))
+print(weather(loc,datetime.datetime.now()))

5a2aafc9d138b359e97e837ea327e9b59c4ca8a9
Zer0-M <mjamil90@stuy.edu>
2019-01-10 00:05:02 -0500

    see devlog for changes

--- util/apihelp.py
+++ util/apihelp.py
@@ -5,14 +5,20 @@ def holidays(year,month):
     '''
         Gets a dictionary of holidays for a certain month and year
     '''
-    r=requests.get("https://holidayapi.pl/v1/holidays?country=US&year={0}&month={1}".format(year,month))
+    url="https://holidayapi.pl/v1/holidays"
+    data={
+        "country":"US",
+        "year":year,
+        "month":month,
+    }
+    r=requests.get(url,params=data)
     try:
-        data=r.json()
+        response=r.json()
     except:
         print("something went wrong")
-    return data
+    return response
 
-#print(holidays(2019,1))
+print(holidays(2019,1))
 
 def datify(time):
     '''
@@ -29,7 +35,6 @@ def weather(location,time):
         The time should be a datetime object
     '''
     time=datify(time)
-    print(time)
     r=requests.get("https://api.darksky.net/forecast/{0}/{1},{2},{3}".format(DarkSkyKey,location[0],location[1],time))
     try:
         data=r.json()
@@ -39,5 +44,40 @@ def weather(location,time):
     weather.append(data["currently"]["summary"])
     weather.append(int(data["currently"]["temperature"]))
     return weather
-loc=(40.717716,-74.014039)
+def getGeocode(address):
+    '''
+        Returns the longitude and latitude for a given address
+    '''
+    url = "https://us1.locationiq.com/v1/search.php"
+    data = {
+        'key': '3228e551e787f2',
+        'q': address,
+        'format': 'json'
+    }
+    r = requests.get(url, params=data)
+    response=r.json()
+    loc=[]
+    loc.append(float(response[0]["lat"]))
+    loc.append(float(response[0]["lon"]))
+    return loc
+def traffic(location):
+    '''
+        Returns traffic incidents near a given location
+    '''
+    url="https://www.mapquestapi.com/traffic/v2/incidents"
+    data={
+        "outFormat":"json",
+        "key":"CSlfJJOM0zpH9dA10zzIJkdDl6stmUtR",
+        "boundingBox":"{0},{1},{2},{3}".format(location[0],location[1],location[0]+0.03,location[1]+0.03),
+        "filters":"incidents,construction,congestion,event"
+    }
+    r=requests.get(url,params=data)
+    response=r.json()
+    incidentlist=[]
+    for incident in response["incidents"]:
+        incidentlist.append(incident["shortDesc"]) 
+    return incidentlist
+
+loc=getGeocode("Stuyvesant High School")
 print(weather(loc,datetime.datetime.now()))
+print(traffic(loc))

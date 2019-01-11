84fd9f6ac0968a4a3e177c68f1b5c70c256d491f
Zer0-M <mjamil90@stuy.edu>
2019-01-11 12:50:41 -0500

    authentication is now functioning

--- app.py
+++ app.py
@@ -6,17 +6,32 @@ import ssl
 
 from flask import Flask, render_template, session, request, url_for, redirect, flash
 
+from util import auth
+
 app = Flask(__name__)
 app.secret_key = os.urandom(32)
 
-@app.route("/")
+@app.route("/",methods=["POST","GET"])
 def home():
-    #if not session.get("username"):
-    #    return redirect(url_for("login"))
+    if "login" in request.form:
+        message=auth.login(request.form["user"],request.form["pass"])
+        flash(message)
+        if message!="Login successful":
+            return redirect(url_for("login"))
+        else:
+            session["username"]=request.form["user"]
+    if "register" in request.form:
+        message=auth.register(request.form["user"],request.form["pass"],request.form["confirmpass"],request.form["display"])
+        flash(message)
+        return redirect(url_for("login"))
+    if "username" not in session:
+        return redirect(url_for("login"))
     return render_template("home.html", todo = [["1", "Doctor's Appointment", "In 3 Hours", "Eye appointment located in Long Island. Lots of Traffic"], ["2","Doctor's Appointment", "In 3 Hours", "Eye appointment located in Long Island. Lots of Traffic"], ["3","Doctor's Appointment", "In 3 Hours", "Eye appointment located in Long Island. Lots of Traffic"]])
 
 @app.route("/login")
 def login():
+    if "username" in session:
+        return redirect(url_for("home"))
     return render_template("login.html")
 
 if __name__== "__main__":
--- util/auth.py
+++ util/auth.py
@@ -3,24 +3,24 @@ import sqlite3
 
 DB_FILE = "data/database.db"
 
-def register(user, pass, conf, display):
+def register(user, password, conf, display):
     db = sqlite3.connect(DB_FILE)
     c = db.cursor()
-    if pass != conf:
+    if password != conf:
         return "Passwords do not match"
     for i in c.execute("SELECT user FROM accts WHERE user = ? ",(user,)):
         db.close()
         return "Username already exists"
-    c.execute("INSERT INTO accts (user, password, display) VALUES(?,?,?)", (user,pwd,display,))
+    c.execute("INSERT INTO accts (user, password, display) VALUES(?,?,?)", (user,password,display,))
     db.commit()
     db.close()
     return "Account creation successful"
 
-def login(user, pass):
+def login(user, password):
     db = sqlite3.connect(DB_FILE)
     c = db.cursor()
     for i in c.execute("SELECT password FROM accts WHERE user = ?",(user,)):
-        if i[0] == pass:
+        if i[0] == password:
             db.close()
             return "Login successful"
         else:

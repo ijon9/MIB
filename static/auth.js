f2520d12f83b466a42ee76886664499288e70fb6
Zer0-M <mjamil90@stuy.edu>
2019-01-11 12:51:51 -0500

    added javascript to show display name

--- static/auth.js
+++ static/auth.js
@@ -3,6 +3,7 @@ var registerbutton=document.getElementById("register");
 var loginbutton=document.getElementById("login");
 var logintab=document.getElementById("logintab");
 var confirm=document.getElementById("confirm");
+var display=document.getElementById("display");
 
 registertab.addEventListener("click",(e) => {
     registertab.className="nav-link active";
@@ -10,11 +11,13 @@ registertab.addEventListener("click",(e) => {
     loginbutton.setAttribute("hidden",true);
     registerbutton.removeAttribute("hidden");
     confirm.removeAttribute("hidden");
+    display.removeAttribute("hidden");
 });
 logintab.addEventListener("click",() => {
     logintab.className="nav-link active";
     registertab.className="nav-link";
     registerbutton.setAttribute("hidden",true);
     confirm.setAttribute("hidden",true);
+    display.setAttribute("hidden",true);
     loginbutton.removeAttribute("hidden");
 });

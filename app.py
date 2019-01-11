e01c9784789182470561b2fcded86525c3d70af6
Britni <bcanale@stuy.edu>
2019-01-10 22:22:31 -0500

    Fixed format, added to home

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app.secret_key = os.urandom(32)
 def home():
     #if not session.get("username"):
     #    return redirect(url_for("login"))
-    return render_template("home.html")
+    return render_template("home.html", todo = [["1", "Doctor's Appointment", "In 3 Hours", "Eye appointment located in Long Island. Lots of Traffic"], ["2","Doctor's Appointment", "In 3 Hours", "Eye appointment located in Long Island. Lots of Traffic"], ["3","Doctor's Appointment", "In 3 Hours", "Eye appointment located in Long Island. Lots of Traffic"]])
 
 @app.route("/login")
 def login():

b41942572bd1b068c74544c9d2cc9cc71c9dc2c8
ijon9 <ijon@stuy.edu>
2019-01-14 12:32:34 -0500

    friend requests page

--- util/friends.py
+++ util/friends.py
@@ -3,16 +3,21 @@ import sqlite3
 
 DB_FILE = "data/database.db"
 
-friend_request(user1, user2):
+def friend_request(user1, user2):
     db = sqlite3.connect(DB_FILE)
     c = db.cursor()
     c.execute("INSERT INTO friends (username, friend, accepted) VALUES (?,?,0)",(user1,user2,))
     db.commit()
     db.close()
 
-accept_friend(user1, user2):
+def accept_friend(user1, user2):
     db = sqlite3.connect(DB_FILE)
     c = db.cursor()
     c.execute("UPDATE friends SET accepted = 1 WHERE user1 == ? AND user2 == ? OR user1 == ? AND user2 == ?",(user1,user2,user2,user1,))
     db.commit()
     db.close()
+
+def friend_list():
+    db = sqlite3.connect(DB_FILE)
+    c = db.cursor()
+    return c.execute("SELECT display FROM accts").fetchall()

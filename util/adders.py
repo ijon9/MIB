5e95f165094651fae63bb8abad5bbee8b0417ad4
Mohammed Uddin <muddin3@stuy.edu>
2019-01-13 20:11:50 -0500

    updated calendar-related database functions to match the calendar table

--- util/adders.py
+++ util/adders.py
@@ -6,8 +6,8 @@ DB_FILE = "data/database.db"
 def add_event(creator, name, time, location, description, public, alert, priority):
     db = sqlite3.connect(DB_FILE)
     c = db.cursor()
-    command = "INSERT INTO calendar (creator, name, time, location, description, public, alert, priority) VALUES (?,?,?,?,?,?,?,?)"
-    args = (creator, name, time, location, description, public, alert, priority)
+    command = "INSERT INTO calendar (creator, name, day, clock, location, description, public, alert, priority) VALUES (?,?,?,?,?,?,?,?,?)"
+    args = (creator, name, day, clock, location, description, public, alert, priority)
     c.execute(command,args)
     db.commit()
     db.close()
--- util/calendar.py
+++ util/calendar.py
@@ -7,6 +7,6 @@ def get_todo(user, day):
     """Returns the to-do list for a specific day"""
     db = sqlite3.connect(DB_FILE)
     c = db.cursor()
-    command = "SELECT name,time,location,description FROM calendar WHERE creator == ? AND day == ?"
+    command = "SELECT name,day,clock,location,description FROM calendar WHERE creator == ? AND day == ?"
     args = (user,day,)
     return c.execute(command,args).fetchall()

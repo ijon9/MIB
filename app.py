cafd3427dc214c5d91cd38f928a2fe89655965a8
Britni <bcanale@stuy.edu>
2019-01-13 20:29:34 -0500

    Chaging calendar to display events

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ import urllib
 import random
 import os
 import ssl
+import datetime
 
 from flask import Flask, render_template, session, request, url_for, redirect, flash
 
-from util import auth
+from util import auth, calendar
 
 app = Flask(__name__)
 app.secret_key = os.urandom(32)
@@ -35,9 +36,21 @@ def login():
     return render_template("login.html")
 
 @app.route("/calendar")
-def calendar():
-    return render_template("calendar.html")
+def cal():
+    date = datetime.date.today().isocalendar()
+    month = calendar.get_calendar(date[0], date[1])
+    return render_template("calendar.html",month = month)
 
+
+@app.route("/logout")
+def logout():
+    try:
+        session.pop('username')
+        flash("You have successfully logged out")
+        return redirect(url_for("login"))
+    except:
+        flash("You have successfully logged out")
+        return redirect(url_for("login"))
 
 if __name__== "__main__":
     app.debug = True
--- util/calendar.py
+++ util/calendar.py
@@ -1,5 +1,5 @@
 from flask import Flask
-import sqlite3
+import sqlite3, calendar
 
 DB_FILE = "data/database.db"
 
@@ -10,3 +10,17 @@ def get_todo(user, day):
     command = "SELECT name,time,location,description FROM calendar WHERE creator == ? AND day == ?"
     args = (user,day,)
     return c.execute(command,args).fetchall()
+
+
+def get_calendar(year, month, user):
+    cal = calendar.Calendar(6).monthdatescalendar(year, month)
+    calendar = []
+    w = 0
+    d = 0
+    for week in cal:
+        calendar[w] = {}
+        w = w+1
+        d = 0
+        for day in week:
+            calendar[w][d] = [day[2], get_todo(user, year, month, day[2])
+            d = d + 1

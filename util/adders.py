b568daa9e25f36c064d002733fcdceeba620391b
Mohammed Uddin <muddin3@stuy.edu>
2019-01-13 20:13:07 -0500

    whoops forgot something

--- util/adders.py
+++ util/adders.py
@@ -3,7 +3,7 @@ import sqlite3
 
 DB_FILE = "data/database.db"
 
-def add_event(creator, name, time, location, description, public, alert, priority):
+def add_event(creator, name, day, clock, location, description, public, alert, priority):
     db = sqlite3.connect(DB_FILE)
     c = db.cursor()
     command = "INSERT INTO calendar (creator, name, day, clock, location, description, public, alert, priority) VALUES (?,?,?,?,?,?,?,?,?)"
